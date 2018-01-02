89683714728aec60f82db0352c1ced223cf61a43
takedai0313 <takedai@live.jp>
2018-01-02 19:14:22 +0000

    JSON.strimngify

--- dywSendLineMsg/index.js
+++ dywSendLineMsg/index.js
@@ -1,4 +1,34 @@
+'use strict';
+
+
+const webclient = require("request");
+const fs = require("fs");
+const url = "https://api.line.me/v2/bot/message/reply";
+const authorization = (JSON.parse(fs.readFileSync(process.env.setting, 'utf8'))).bearer;
+
 exports.handler = (event, context, callback) => {
+    console.log(event);
     // TODO implement
-    callback();
+    var param = {
+        "replyToken": event.replyToken,
+        "messages": [
+            {
+                "type": "text",
+                "text": event.msg
+            }
+        ]
+    };
+    
+    webclient.post({
+        url: url,
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "Authorization": authorization
+        },
+        body: JSON.stringify(param)
+    }, (err, response, body) => {
+        if(err) console.log(err);
+        else console.log("response success" + body);
+    });
+    callback(null);
 };
